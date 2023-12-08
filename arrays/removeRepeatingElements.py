662e905c867c736d236379dfb413f702f609dbdd
AFRID SHAIK <afridayan01@gmail.com>
2023-12-08 19:28:47 +0530

    removeRepeatingElements

--- arrays/removeRepeatingElements.py
+++ arrays/removeRepeatingElements.py
@@ -5,11 +5,9 @@ def removeRepeating(array):
             if(len(emptyArray) >= 1):
                 emptyArray.append(i)
                 for j in range(0,len(emptyArray)-1):
-                    key = emptyArray[j]
                     o = j
                     for k in range(j+1,len(emptyArray)-1):
-                        if key > emptyArray[k]:
-                            key = emptyArray[k]
+                        if emptyArray[k] < emptyArray[o]:
                             o = k
                     emptyArray[j],emptyArray[o] = emptyArray[o],emptyArray[j]
             else:
@@ -19,4 +17,3 @@ if __name__ == "__main__":
     array = [2,3,4,2,7,3,5,8,4,7,4,7]
     result = removeRepeating(array)
     print(result)
-    
