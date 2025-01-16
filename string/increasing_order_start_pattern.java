5cd47b2f0c3c9dc7e4931c2996b5b18cd04ded52
SHIAK AFRID <afridayan01@gmail.com>
2025-01-16 17:50:27 +0530

    Update increasing_order_start_pattern.java

--- string/increasing_order_start_pattern.java
+++ string/increasing_order_start_pattern.java
@@ -1,8 +1,6 @@
 package example;
 
 public class Main {
-	
-	
 	public static void main(String[] args){
 		for(int i = 1 ;i <= 5;i++){
 			for(int j = 1;j<=i;j++){
@@ -10,10 +8,5 @@ public class Main {
 			}
 			System.out.println();
 		}
-		
-		
-		
 	}
-	
-
 }
